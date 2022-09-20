57fe63c35cfb0590f04d1c1792b5c6495eed3e4d
sputnikel0221 <111990655+sputnikel0221@users.noreply.github.com>
2022-09-20 10:49:16 +0900

    CharacterMove Rbody로 구현해보기

--- Assets/CharacterMove.cs
+++ Assets/CharacterMove.cs
@@ -13,15 +13,23 @@ public float speed = 2;
 float vx=0;
 float vy=0;
 bool leftflag = false;
+Rigidbody2D rbody;
+
+    void Start() {
+        //이 스크립트가 적용된 객체에서 Component를 가져오는데, <Rigidbody2D>라는 component를 가져와라 그리고 rbody에 저장한다.
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.gravityScale = 0;
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        //다른 rigidbody 설정도 수동 설정이 가능하다.
+        rbody.sleepMode = RigidbodySleepMode2D.StartAwake;
+        rbody.interpolation = RigidbodyInterpolation2D.None;
+    }
 
 
-    //update에서 설정을 하고, fixedupdate에서 해당 설정대로 실행하는 원리.
-    //update에서 계속 키를 입력받았는지 if문을 돌리면서 대기를 하며, 해당 과정을 수행 
+    // update에서 설정을 하고, fixedupdate에서 해당 설정대로 실행하는 원리.
+    // update에서 계속 키를 입력받았는지 if문을 돌리면서 대기를 하며, 해당 과정을 수행 
     void Update()
     {
-        //why do this
-        
-
         if(Input.GetKey("right")){
             vx = speed;
             leftflag = false;
@@ -31,16 +39,16 @@ bool leftflag = false;
             leftflag = true;
         }
          if(Input.GetKey("up")){
-            vy = speed*3;
+            vy = speed;
         }
-        // if(Input.GetKey("down")){
-        //     vy = -speed;
-        // }
+        if(Input.GetKey("down")){
+            vy = -speed;
+        }
     }
     
 
     void FixedUpdate() {
-        this.transform.Translate(vx/50,vy/50,0);
+        rbody.velocity = new Vector2(vx/50, vy/50);
         this.GetComponent<SpriteRenderer>().flipX = leftflag;
         vx = 0;
         vy = 0;
